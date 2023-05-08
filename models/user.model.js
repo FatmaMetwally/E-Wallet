4352e52a5521a9799d8c20c774d49106b7d768db
Fatma Metwally <61696377+FatmaMetwally@users.noreply.github.com>
2023-05-08 17:52:25 +0300

    Update user.model.js
    updated user to make him apple to add childs
    added role and child array

--- models/user.model.js
+++ models/user.model.js
@@ -1,12 +1,20 @@
 const mongoose = require('mongoose');
-const userchema= mongoose.Schema({
-     name:String,
-     phone:String,
-     NID:String,
-     age:String,
-     address:String,
-     gender:String,
-     password:String
+const userchema = mongoose.Schema({
+     name: String,
+     phone: String,
+     NID: String,
+     age: String,
+     address: String,
+     gender: String,
+     password: String,
+     balance: Number,
+     child: [{type: mongoose.Schema.ObjectId, ref: 'user'}],
+     role: {
+          type: String,
+          enum: ['parent', 'child'],
+          required: true,
+          default: 'parent'
+     }
 
 })
-module.exports= mongoose.model('user',userchema)
+module.exports = mongoose.model('user', userchema)
